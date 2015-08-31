574fb6c02b270dc218fd01e800d8f1e4477611a1
patrickyoreilly <aleph@auk.alephobjects.com>
2015-08-31 12:24:34 -0600

    Changed probe speed.
    -Decreased the speed of second probe.

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -1084,7 +1084,7 @@ static void run_z_probe() {
     st_synchronize();
 
     // move back down slowly to find bed
-    feedrate = homing_feedrate[Z_AXIS]/4;
+    feedrate = homing_feedrate[Z_AXIS]/40;
     zPosition -= home_retract_mm(Z_AXIS) * 2;
     plan_buffer_line(current_position[X_AXIS], current_position[Y_AXIS], zPosition, current_position[E_AXIS], feedrate/60, active_extruder);
     st_synchronize();
