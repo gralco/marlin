5fa147ec36c5eed7a898c71177806c505c87f9ce
patrickaleph <patrick@alephobjects.com>
2015-10-20 10:30:05 -0600

    Incremented eeprom version number, changed M115 information.
    -fixed formating in marlin was an extra space
    -removed UUID from M115 command
    -changed version number to 1.0.3m the m stands for mini
    -increased version number for eeprom from current mini firmware

--- Marlin/language.h
+++ Marlin/language.h
@@ -49,7 +49,7 @@
   #define FIRMWARE_URL "https://github.com/fmalpartida/Marlin/tree/SAV-MkI-config"
 #elif MB(MINI_RAMBO)
   #define MACHINE_NAME "LulzBot Mini"
-  #define FIRMWARE_URL  "https://code.alephobjects.com/diffusion/MARLIN/repository/Foxglove/"
+  #define FIRMWARE_URL "https://code.alephobjects.com/diffusion/MARLIN/repository/Foxglove/"
 #else
   #ifdef CUSTOM_MENDEL_NAME
     #define MACHINE_NAME CUSTOM_MENDEL_NAME
@@ -110,7 +110,7 @@
 #define MSG_HEATING_COMPLETE                "Heating done."
 #define MSG_BED_HEATING                     "Bed Heating."
 #define MSG_BED_DONE                        "Bed done."
-#define MSG_M115_REPORT                     "FIRMWARE_NAME:Marlin V1.0.2; Sprinter/grbl mashup for gen6 FIRMWARE_URL:" FIRMWARE_URL " PROTOCOL_VERSION:" PROTOCOL_VERSION " MACHINE_TYPE:" MACHINE_NAME " EXTRUDER_COUNT:" STRINGIFY(EXTRUDERS) " UUID:" MACHINE_UUID "\n"
+#define MSG_M115_REPORT                     "FIRMWARE_NAME:Marlin V1.0.3m; Sprinter/grbl mashup for gen6 FIRMWARE_URL:" FIRMWARE_URL " PROTOCOL_VERSION:" PROTOCOL_VERSION " MACHINE_TYPE:" MACHINE_NAME " EXTRUDER_COUNT:" STRINGIFY(EXTRUDERS) "\n"
 #define MSG_COUNT_X                         " Count X: "
 #define MSG_ERR_KILLED                      "Printer halted. kill() called!"
 #define MSG_ERR_STOPPED                     "Printer stopped due to errors. Fix the error and use M999 to restart. (Temperature is reset. Set it after restarting)"
