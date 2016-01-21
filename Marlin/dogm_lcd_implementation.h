3806fff49460e3f29e91fee240c959c76234e1fc
gralco <kuzmenko@alephobjects.com>
2016-01-21 11:46:13 -0700

    Initial flexystruder setup

--- Marlin/dogm_lcd_implementation.h
+++ Marlin/dogm_lcd_implementation.h
@@ -125,9 +125,9 @@ static void lcd_implementation_init()
 			u8g.drawBitmapP(0,0,LULZBOT_BMPBYTEWIDTH,LULZBOT_BMPHEIGHT,lulzbot_bmp);
 			// Welcome message
 			u8g.setFont(u8g_font_6x10_marlin);
-			u8g.drawStr(61,17,"TAZ 6"); 
+			u8g.drawStr(61,17,"TAZ 6");
 			u8g.setFont(u8g_font_5x8);
-			u8g.drawStr(95,17,"Single");
+			u8g.drawStr(95,17,"Flexy");
 			u8g.setFont(u8g_font_6x10_marlin);
 			u8g.drawStr(62,28,"3D Printer");
 			u8g.setFont(u8g_font_5x8);
