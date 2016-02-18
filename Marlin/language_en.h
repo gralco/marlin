869a11fd4ceeba5218624eba458e4a2f84ea970d
gralco <kuzmenko@alephobjects.com>
2016-02-18 08:26:26 -0700

    Revert "State Nozzle1 for E0 temp in the Tune menu"
    This reverts commit 36cac3ce03a8ee6c426f4b258c0e7e3aeeb7aeb5.

--- Marlin/language_en.h
+++ Marlin/language_en.h
@@ -63,7 +63,7 @@
 #define MSG_MOVE_1MM                        "Move 1mm"
 #define MSG_MOVE_10MM                       "Move 10mm"
 #define MSG_SPEED                           "Speed"
-#define MSG_NOZZLE                          "Nozzle1"
+#define MSG_NOZZLE                          "Nozzle"
 #define MSG_NOZZLE1                         "Nozzle2"
 #define MSG_NOZZLE2                         "Nozzle3"
 #define MSG_BED                             "Bed"
