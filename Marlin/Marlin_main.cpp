5af3197f9ac24362ffbd556ab803b9497a6c8e92
gralco <kuzmenko@alephobjects.com>
2016-03-02 12:25:34 -0700

    Bring both extruders back to 0 after probe fail

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -2197,9 +2197,14 @@ void process_commands()
               reprobe_attempts[0] = 0;
               reprobe_attempts[1] = 0;
               do_blocking_move_to(current_position[X_AXIS], current_position[Y_AXIS], 10.0);
+              active_extruder = 0;
               plan_buffer_line(current_position[X_AXIS], current_position[Y_AXIS], current_position[Z_AXIS], 0.0, feedrate/60, active_extruder);
               st_synchronize();
-              plan_set_position(current_position[X_AXIS], current_position[Y_AXIS], current_position[Z_AXIS], current_position[E_AXIS]);
+              active_extruder = 1;
+              plan_buffer_line(current_position[X_AXIS], current_position[Y_AXIS], current_position[Z_AXIS], 15.0, feedrate/60, active_extruder);
+              st_synchronize();
+              active_extruder = 0;
+              plan_set_position(current_position[X_AXIS], current_position[Y_AXIS], current_position[Z_AXIS], 0.0);
               acceleration = DEFAULT_ACCELERATION;
               tone(BEEPER, 1750);
               delay(750);
