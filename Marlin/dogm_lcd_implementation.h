596a273f57e11509439500749e301be799e09cff
gralco <kuzmenko@alephobjects.com>
2016-03-18 09:03:13 -0600

    Include the version number on the splash screen

--- Marlin/dogm_lcd_implementation.h
+++ Marlin/dogm_lcd_implementation.h
@@ -140,7 +140,7 @@ static void lcd_implementation_init()
 			u8g.drawStr(58,41,"LulzBot.com");
 			u8g.setFont(u8g_font_5x8);
 			u8g.drawStr(58,53,"Firmware:");
-			u8g.drawStr(58,62,"Marlin 2015Q3");
+			u8g.drawStr(58,62,"Marlin 1.0.0.1");
 	   } while( u8g.nextPage() );
 delay(3000);
 }
