47dda1a75627e4466f960a489cbd153d7256bbea
gralco <kuzmenko@alephobjects.com>
2016-07-19 15:50:00 -0600

    Increment the version number to 1.1.0.5

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -29,7 +29,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "1.1.0.4 " //based off of the upstream RCBugFix branch
+#define SHORT_BUILD_VERSION "1.1.0.5 " //based off of the upstream RCBugFix branch
 
 /**
  * Verbose version identifier which should contain a reference to the location
