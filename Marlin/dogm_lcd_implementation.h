d3d9864e2d6d58cb2dca2d0322feeb852746bee7
patrickaleph <patrick@alephobjects.com>
2016-07-28 14:20:27 -0600

    Update splash screen with opahs new name MOAR struder

--- Marlin/dogm_lcd_implementation.h
+++ Marlin/dogm_lcd_implementation.h
@@ -127,9 +127,9 @@ static void lcd_implementation_init()
 			u8g.setFont(u8g_font_6x10_marlin);
 			u8g.drawStr(61,17,"TAZ 6"); 
 			u8g.setFont(u8g_font_5x8);
-			u8g.drawStr(95,17,"BigNoz");
-			u8g.setFont(u8g_font_6x10_marlin);
-			u8g.drawStr(62,28,"3D Printer");
+			u8g.drawStr(95,17,"Single");
+			u8g.setFont(u8g_font_5x8);
+			u8g.drawStr(62,28,"MOAR struder");
 			u8g.setFont(u8g_font_5x8);
 			u8g.drawStr(63,41,"LulzBot.com");
 			u8g.setFont(u8g_font_5x8);
