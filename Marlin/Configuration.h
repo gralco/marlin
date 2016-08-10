0eadc1a28aef01ddc64d33cdd4117174c089642e
patrickaleph <patrick@alephobjects.com>
2016-08-10 11:09:33 -0600

    Change marlin version number, decrease z max movement to allow for homing movement.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -345,7 +345,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define X_MIN_POS 0
 #define Y_MAX_POS 275
 #define Y_MIN_POS 0
-#define Z_MAX_POS 250
+#define Z_MAX_POS 242
 #define Z_MIN_POS 0
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
--- Marlin/dogm_lcd_implementation.h
+++ Marlin/dogm_lcd_implementation.h
@@ -141,7 +141,7 @@ static void lcd_implementation_init()
 			u8g.setFont(u8g_font_5x8);
 			u8g.drawStr(63,53,"Firmware:");
 			u8g.drawStr(63,62,"Marlin");
-			u8g.drawStr(94,62,"1.0.0.1");
+			u8g.drawStr(94,62,"1.0.0.2");
 	   } while( u8g.nextPage() );
 delay(3000);
 }
