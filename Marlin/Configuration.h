a49a09811fc897e495acd9f66e80cdcc60b2935d
patrickaleph <patrick@alephobjects.com>
2016-08-11 11:57:28 -0600

    Edit comment with with new toolhead name MOARstruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -163,7 +163,7 @@
   #define PID_dT ((OVERSAMPLENR * 10.0)/(F_CPU / 64.0 / 256.0)) //sampling period of the temperature routine
 
 // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
-// LulzBot Hellish Longblock (24v)
+// LulzBot MOARstruder (40v)
       #define DEFAULT_Kp 55.64
       #define DEFAULT_Ki 6.79
       #define DEFAULT_Kd 113.94
