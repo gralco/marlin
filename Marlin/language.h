ac3eff8c63b004b771f4afb68e576ddaf68e9faf
patrickaleph <patrick@alephobjects.com>
2016-08-12 11:03:32 -0600

    Update M115 information

--- Marlin/language.h
+++ Marlin/language.h
@@ -36,7 +36,7 @@
 	#ifdef CUSTOM_PRINTER_NAME
 		#define MACHINE_NAME CUSTOM_PRINTER_NAME
 	#else
-		#define MACHINE_NAME "RepRap"
+		#define MACHINE_NAME "LulzBot TAZ 4 5"
 	#endif
 
 	#define FIRMWARE_URL "https://code.alephobjects.com/diffusion/MARLIN/"
@@ -191,7 +191,7 @@
 	#define MSG_HEATING_COMPLETE "Heating done."
 	#define MSG_BED_HEATING "Bed Heating."
 	#define MSG_BED_DONE "Bed done."
-	#define MSG_M115_REPORT "FIRMWARE_NAME:Marlin V1.0.0.1; Standard V2.0 FIRMWARE_URL:" FIRMWARE_URL " PROTOCOL_VERSION:" PROTOCOL_VERSION " MACHINE_TYPE:" MACHINE_NAME " EXTRUDER_COUNT:" STRINGIFY(EXTRUDERS) " UUID:" MACHINE_UUID "\n"
+	#define MSG_M115_REPORT "FIRMWARE_NAME:Marlin V1.0.0.2; Standard V2.0 FIRMWARE_URL:" FIRMWARE_URL " PROTOCOL_VERSION:" PROTOCOL_VERSION " MACHINE_TYPE:" MACHINE_NAME " EXTRUDER_COUNT:" STRINGIFY(EXTRUDERS) " UUID:" MACHINE_UUID "\n"
 	#define MSG_COUNT_X " Count X: "
 	#define MSG_ERR_KILLED "Printer halted. kill() called!"
 	#define MSG_ERR_STOPPED "Printer stopped due to errors. Fix the error and use M999 to restart. (Temperature is reset. Set it after restarting)"
